b774e8950b2e7ed435d93660e7ae326c879c6780
Fabio Biondi <info@fabiobiondi.com>
2019-10-21 04:24:25 +0200

    Update layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,25 @@ import { RXJS_MEETUP_CODEMOTION } from './data/rxjs-meetup-codemotion';
           <img [src]="'assets/' + speaker.thumb" width="100%" style="max-width: 250px">
         </div>
         
-        <div *ngFor="let snippet of speaker.snippets">
-          <h6>{{snippet.title}}</h6>
+        <div *ngFor="let snippet of speaker.snippets; let i = index">
+          <h5>{{i + 1}}) {{snippet.title}}</h5>
           
           <div class="d-flex justify-content-between">
             <div>
-              <a [href]="urlFinal" *ngIf="snippet.urlFinal as urlFinal">
+              <a 
+                *ngIf="snippet.urlFinal as urlFinal" 
+                class="text-secondary"
+                [href]="urlFinal" 
+              >
                 <i class="fa fa-code"></i> Source Code
               </a>
             </div>
             <div>
-              <a [href]="urlStart" *ngIf="snippet.urlStart as urlStart">
+              <a 
+                *ngIf="snippet.urlStart as urlStart" 
+                class="text-success"
+                [href]="urlStart" 
+              >
                 <i class="fa fa-play-circle"></i> Playground
               </a>
             </div>
